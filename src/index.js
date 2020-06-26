f2d5a9f7b9f659b8b59a6cf81d38ee38fc1f57b7
Brian Ton <BrianTon@MacBook-Pro.local>
2020-06-26 11:05:49 -0700

    formatting changes

--- src/index.js
+++ src/index.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 
 class Task extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            title: "Task Title",
-            type: "Task",
-            assignee: "John Doe"
-        };
-    }
-    
-    render() {
-        return (
-            <div class="task-card">
-                <div class="task-title">{this.title}</div>
-                <div class="task-type">{this.type}</div>
-                <div class="task-assignee">{this.assignee}</div>
-            </div>
-        );
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: "Task Title",
+      type: "Task",
+      assignee: "John Doe",
+    };
+  }
+
+  render() {
+    return (
+      <div class="task-card">
+        <div class="task-title">{this.title}</div>
+        <div class="task-type">{this.type}</div>
+        <div class="task-assignee">{this.assignee}</div>
+      </div>
+    );
+  }
 }
 
 // ========================================
