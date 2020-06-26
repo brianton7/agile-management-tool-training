5dc5a18a84cb14b307543903e4a5fb847eb45c13
Brian Ton <BrianTon@MacBook-Pro.local>
2020-06-26 14:37:04 -0700

    Added render methods for type, title, assignee

--- src/index.js
+++ src/index.js
@@ -2,6 +2,24 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
+function Title(props) {
+  return (
+    <h1>{props.value}</h1>
+  );
+}
+
+function Type(props) {
+  return (
+    <p>{props.value}</p>
+  );
+}
+
+function Assignee(props) {
+  return (
+    <p>{props.value}</p>
+  );
+}
+
 class Task extends React.Component {
   constructor(props) {
     super(props);
@@ -12,12 +30,46 @@ class Task extends React.Component {
     };
   }
 
+  renderTitle(title) {
+    return (
+      <Title 
+        value={title}
+      />
+    );
+  }
+
+  renderType(type) {
+    return (
+      <Type
+        value={type}
+      />
+    );
+  }
+
+  renderAssignee(assignee) {
+    return (
+      <Assignee
+        value={assignee}
+      />
+    );
+  }
+
   render() {
+    const title = this.state.title;
+    const type = this.state.type;
+    const assignee = this.state.assignee;
+    
     return (
       <div class="task-card">
-        <div class="task-title">{this.title}</div>
-        <div class="task-type">{this.type}</div>
-        <div class="task-assignee">{this.assignee}</div>
+        <div class="task-title">
+          {this.renderTitle(title)}
+        </div>
+        <div class="task-type">
+          {this.renderType(type)}
+        </div>
+        <div class="task-assignee">
+          {this.renderAssignee(assignee)}
+        </div>
       </div>
     );
   }
