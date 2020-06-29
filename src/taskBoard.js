8dc619dff680cd07fb0da2678099fe7ad7fce1d8
Brian Ton <BrianTon@MacBook-Pro.local>
2020-06-29 13:40:10 -0700

    Updated task board class render function

--- src/taskBoard.js
+++ src/taskBoard.js
@@ -2,9 +2,26 @@ import React from "react";
 import TaskCard from "./taskCard.js"
 
 function TaskColumn(props) {
-  
+  return (
+    <div class="task-column">
+      <h2>{props.value}</h2>
+      <TaskCard />
+      <TaskCard />
+    </div>
+  );
 }
 
 class TaskBoard extends React.Component {
-
+  render() {
+    return (
+      <div class="board">
+        <TaskColumn value="TO DO" />
+        <TaskColumn value="IN PROGRESS" />
+        <TaskColumn value="READY TO TEST" />
+        <TaskColumn value="DONE" />
+      </div>
+    );
+  }
 }
+
+export default TaskBoard;
