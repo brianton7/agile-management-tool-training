4987cb91d2ec8ebc80bcb688cab84fc9154857ed
Brian Ton <BrianTon@MacBook-Pro.local>
2020-06-29 09:56:52 -0700

    Rearranged files to add taskboard

---
+++ src/taskBoard.js
@@ -0,0 +1,10 @@
+import React from "react";
+import TaskCard from "./taskCard.js"
+
+function TaskColumn(props) {
+  
+}
+
+class TaskBoard extends React.Component {
+
+}
---
+++ src/taskCard.js
@@ -0,0 +1,75 @@
+import React from "react";
+import "./taskCard.css";
+
+function Title(props) {
+  return (
+    <h1>{props.value}</h1>
+  );
+}
+
+function Type(props) {
+  return (
+    <p>{props.value}</p>
+  );
+}
+
+function Assignee(props) {
+  return (
+    <p>{props.value}</p>
+  );
+}
+
+class TaskCard extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: "Task Title",
+      type: "Task",
+      assignee: "John Doe",
+    };
+  }
+
+  renderTitle(title) {
+    return (
+      <Title 
+        value={title}
+      />
+    );
+  }
+
+  renderType(type) {
+    return (
+      <Type
+        value={type}
+      />
+    );
+  }
+
+  renderAssignee(assignee) {
+    return (
+      <Assignee
+        value={assignee}
+      />
+    );
+  }
+
+  render() {
+    const title = this.state.title;
+    const type = this.state.type;
+    const assignee = this.state.assignee;
+    
+    return (
+      <div class="task-card">
+        <div class="task-title">
+          {this.renderTitle(title)}
+        </div>
+        <div class="task-type">
+          {this.renderType(type)}
+        </div>
+        <div class="task-assignee">
+          {this.renderAssignee(assignee)}
+        </div>
+      </div>
+    );
+  }
+}
